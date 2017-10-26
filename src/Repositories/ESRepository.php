c9b7e189f300735e14dda0f6a4288e46aa044c9c
Mustafa Hussain <mustafa.hussain93@gmail.com>
2017-10-26 15:28:43 +0200

    create ES Repository functionalty

--- src/Repositories/ESRepository.php
+++ src/Repositories/ESRepository.php
@@ -2,9 +2,95 @@
 
 namespace AqarmapESRepository\Repositories;
 
-use AqarmapESRepository\Repositories\BaseSearchRepository;
+use Elastica\Query;
 
 class ESRepository extends BaseSearchRepository
 {
 
+    /**@var array $sortBy*/
+    protected $sortBy;
+
+    /**@var array $order*/
+    protected $order;
+
+    /**@var Query $finalQuery*/
+    protected $finalQuery;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->finalQuery = new Query();
+        $this->sortBy = ['_score'];
+        $this->order = [];
+    }
+    /**
+     * @param array $sortBy
+     * @return ESRepository
+     */
+    public function setSort(array $sortBy)
+    {
+        $this->sortBy = $sortBy;
+        return $this;
+    }
+
+    /**
+     * @param array $order
+     * @return ESRepository
+     */
+    public function setOrder(array $order)
+    {
+        $this->order = $order;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSortBy()
+    {
+        return $this->sortBy;
+    }
+
+    public function getResultWithScore(Query\FunctionScore $functionScore)
+    {
+        return $this->executeCallback(
+            get_called_class(),
+            __FUNCTION__,
+            func_get_args(),
+            function () use ($functionScore) {
+                return $this->scoreResults($functionScore);
+            }
+        );
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResult()
+    {
+        return $this->executeCallback(get_called_class(), __FUNCTION__, func_get_args(), function () {
+            return $this->finalQuery->setQuery($this->prepareQuery())->setSort($this->getSortBy());
+        });
+    }
+
+    /**
+     * @param Query\FunctionScore $functionScore
+     * @return Query
+     */
+    public function scoreResults(Query\FunctionScore $functionScore)
+    {
+        $functionScore->setQuery($this->prepareQuery());
+        $this->finalQuery->setQuery($functionScore);
+        $this->finalQuery->setSort($this->getSortBy());
+
+        return $this->finalQuery;
+    }
 }
