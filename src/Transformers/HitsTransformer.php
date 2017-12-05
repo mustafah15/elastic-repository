b19dc531c2059f3930f1a7b1f5e6c6c8b15629f4
Mustafa Hussain <mustafa.hussain93@gmail.com>
2017-12-05 12:22:40 +0200

    add hitsttransformer

--- src/Transformers/HitsTransformer.php
+++ src/Transformers/HitsTransformer.php
@@ -2,7 +2,16 @@
 
 namespace AqarmapESRepository\Transformers;
 
-class HitsTransformer
-{
+use AqarmapESRepository\Contracts\TransformerContract;
 
+class HitsTransformer implements TransformerContract
+{
+    /**
+     * @param array $results
+     * @return array|mixed
+     */
+    public function transform(array $results)
+    {
+        return $results['hits']['hits'];
+    }
 }
