1cac06d6e1a1358ebcc380dc03997101f45799fe
Mustafa Hussain <mustafa.hussain93@gmail.com>
2017-12-05 15:07:31 +0200

    add finder class implementation

--- src/Finders/Finder.php
+++ src/Finders/Finder.php
@@ -1,7 +1,47 @@
 <?php
 namespace AqarmapESRepository\Finders;
 
-class Finder
-{
+use AqarmapESRepository\Contracts\FinderContract;
+use AqarmapESRepository\Contracts\TransformerContract;
+use AqarmapESRepository\Transformers\HitsTransformer;
+use Elastica\Client;
+use Elastica\Query;
+use Elastica\Type;
 
+class Finder implements FinderContract
+{
+    /**@var  TransformerContract*/
+    protected $transformer;
+
+    /**@var Client */
+    protected $client;
+
+    public function __construct(Client $client)
+    {
+        $this->transformer = new HitsTransformer();
+        $this->client = $client;
+    }
+
+    /**
+     * find result for a query
+     * @param Query $query
+     * @param Type $type
+     * @return array
+     */
+    public function find(Query $query, Type $type)
+    {
+        $path = $this->generateRequestPath($type);
+
+        return $this->client->request($path, "GET", $query->toArray())->getData();
+    }
+
+    /**
+     * generate request path
+     * @param Type $type
+     * @return string
+     */
+    public function generateRequestPath(Type $type)
+    {
+        return $type->getIndex()->getName(). '/' .$type->getName(). '/_search';
+    }
 }
