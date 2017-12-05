4b4a2229219cb6b429208ecd6688bca62e41b094
Mustafa Hussain <mustafa.hussain93@gmail.com>
2017-12-05 13:18:08 +0200

    add finder test

---
+++ tests/Finders/FinderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Finders;
+
+use AqarmapESRepository\Finders\Finder;
+use Elastica\Client;
+use Elastica\Type;
+use Elastica\Index;
+use PHPUnit\Framework\TestCase;
+
+class FinderTest extends TestCase
+{
+    /**@var Finder */
+    protected $finder;
+    protected $path;
+    protected $type;
+    public function setUp()
+    {
+        $this->finder = new Finder(new Client);
+        $this->type = new Type(new Index(new Client, 'indexName'),'typeName');
+        $this->path = $this->finder->generateRequestPath($this->type);
+    }
+
+    public function test_path_suffix()
+    {
+        $this->assertStringEndsWith('_search', $this->path);
+    }
+
+    public function test_path_prefix()
+    {
+        $this->assertStringStartsWith('indexName', $this->path);
+    }
+}
