add80a7a10066fe9577f14b1e1b8f0cbdf810e85
Mustafa Hussain <mustafa.hussain93@gmail.com>
2017-12-05 15:00:49 +0200

    add transformer tests

---
+++ tests/Transformers/HitsTransformerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Transformers;
+
+use AqarmapESRepository\Transformers\HitsTransformer;
+use phpDocumentor\Reflection\Types\Array_;
+use PHPUnit\Framework\TestCase;
+
+class HitsTransformerTest extends TestCase
+{
+    protected $transformer;
+    protected $queryArray;
+    public function setUp()
+    {
+        $this->transformer = new HitsTransformer();
+        $this->queryArray = array('hits'=>['hits'=>[]]);
+    }
+
+    public function test_it_retrun_array()
+    {
+        $this->assertTrue(is_array($this->transformer->transform($this->queryArray)));
+    }
+}
